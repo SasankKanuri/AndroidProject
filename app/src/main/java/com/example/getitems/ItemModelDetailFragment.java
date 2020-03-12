6baf886549eeaf97f85b450afef5d9517f6e76ba
Sasank Kanuri <SasankKanuri@users.noreply.github.com>
2020-03-11 20:33:13 -0700

    Added images and updated styles to match my game.

--- app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
+++ app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
@@ -1,6 +1,8 @@
 package com.example.getitems;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -10,9 +12,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.getitems.ItemIndexViewModel.ItemModel;
+import com.example.getitems.models.ItemViewModel;
 
 /**
  * A fragment representing a single ItemModel detail screen.
@@ -30,7 +34,7 @@ public class ItemModelDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private ItemModel.DummyItem mItem;
+    private ItemViewModel.ItemModel mItem;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -47,24 +51,41 @@ public class ItemModelDetailFragment extends Fragment {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
-            mItem = ItemModel.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            mItem = ItemViewModel.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
+                appBarLayout.setTitle(mItem.Name);
+
+                // The id on the layout
+                ImageView image = ((ImageView) activity.findViewById(R.id.itemmodel_URI));
+
+                // Get the image from the drawable folder
+                String uri = "@drawable/" + mItem.ImageURI;  // where myresource (without the extension) is the file
+                int imageResource = getResources().getIdentifier(uri, "drawable", "com.example.getitems");
+                Drawable res = getResources().getDrawable(imageResource);
+                image.setImageDrawable(res);
             }
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.itemmodel_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.itemmodel_detail)).setText(mItem.details);
+
+            // Show all the Fields
+            ((TextView) rootView.findViewById(R.id.ItemDetails_NameValue)).setText(mItem.Name);
+            ((TextView) rootView.findViewById(R.id.ItemDetails_DescriptionValue)).setText(mItem.Description);
+            ((TextView) rootView.findViewById(R.id.ItemDetails_RangeValue)).setText(String.valueOf(mItem.Range));
+            ((TextView) rootView.findViewById(R.id.ItemDetails_ValueValue)).setText(String.valueOf(mItem.Value));
+            ((TextView) rootView.findViewById(R.id.ItemDetails_DamageValue)).setText(String.valueOf(mItem.Damage));
+            ((TextView) rootView.findViewById(R.id.ItemDetails_LocationValue)).setText(mItem.Location.toString());
         }
 
         return rootView;
